e1944136e2171ebebb82a17df86e8154760a58cc
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-05-26 00:43:14 -0300

    Alteraçao

--- src/controllers/newBlogPost.js
+++ src/controllers/newBlogPost.js
@@ -1,18 +1,22 @@
 const newBlog = require('../services/newBlogPost');
 
 const newBlogPostController = async (req, res, next) => {
-    try {
-        const { title, content, categoryIds } = req.body;
-        // console.log(title, content, categoryIds, 'dados body');
-        const { email } = req.user;
-        // console.log(email, 'email controller');
-        const newBlogPostCreated = await newBlog
-            .newBlogPostService({ title, content, categoryIds, email });
+  try {
+    const { title, content, categoryIds } = req.body;
 
-       return res.status(201).json(newBlogPostCreated);
-    } catch (err) {
-        next(err);
-    }
+    const { email } = req.user;
+
+    const newBlogPostCreated = await newBlog.newBlogPostService({
+      title,
+      content,
+      categoryIds,
+      email,
+    });
+
+    return res.status(201).json(newBlogPostCreated);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
--- src/services/newBlogPost.js
+++ src/services/newBlogPost.js
@@ -1,5 +1,4 @@
 const { Category, BlogPost, User } = require('../database/models');
-// require('dotenv').config();
 
 const erroHandler = (status, message) => ({
   status,
@@ -8,22 +7,18 @@ const erroHandler = (status, message) => ({
 
 const newBlogPostService = async ({ title, content, categoryIds, email }) => {
   const findUser = await User.findOne({ where: { email }, attributes: ['id'] });
-  // console.log(findUser, 'passei aqui');
+
   const { id } = findUser.dataValues;
-  // console.log(id, 'id');
 
   const categories = await Category.findAll({ where: { id: categoryIds } });
-  //   console.log(newBlog, 'newBlog teste');
 
   if (categories.length !== categoryIds.length) {
     throw erroHandler(400, '"categoryIds" not found');
   }
 
   const newBlogPost = await BlogPost.create({ title, content, userId: id });
-  console.log(newBlogPost, 'teste');
-  // console.log(newBlogPost, 'passei aqui');
-    await newBlogPost.addCategories(categoryIds);
-  //  console.log(teste, 'teste');
+
+  await newBlogPost.addCategories(categoryIds);
 
   return newBlogPost;
 };
